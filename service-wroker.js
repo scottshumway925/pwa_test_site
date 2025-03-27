d13262e6af22cf2619f522ff6e254fd9acdc26ad
scottshumway925 <shumworld925@gmail.com>
2025-03-27 10:26:44 -0600

    pwatest website

---
+++ service-wroker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "pwa-cache-v1";
+const urlsToCache = [
+    "/",
+    "/index.html",
+    "/static/styles.css",
+    "/static/script.js",
+    "/manifest.json",
+    "/static/icons/icon-192x192.png",
+    "/static/icons/icon-512x512.png"
+];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
